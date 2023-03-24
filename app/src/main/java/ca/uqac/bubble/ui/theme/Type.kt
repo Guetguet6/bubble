9eaf8c23029b32cbfc206a1cf924f18cdda2c776
Gaétan Dubois <gaetan_dubois1@ens.univ-artois.fr>
2023-03-23 22:29:24 -0400

    :lipstick: Ajout de la Police Quicksand ainsi que du Logo

--- app/src/main/java/ca/uqac/bubble/ui/theme/Type.kt
+++ app/src/main/java/ca/uqac/bubble/ui/theme/Type.kt
@@ -2,16 +2,26 @@ package ca.uqac.bubble.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ca.uqac.bubble.R
+
+val QuickSand = FontFamily(
+    Font(R.font.quicksand_light, FontWeight.Light),
+    Font(R.font.quicksand_medium, FontWeight.Medium),
+    Font(R.font.quicksand_regular, FontWeight.Normal),
+    Font(R.font.quicksand_semibold, FontWeight.SemiBold),
+    Font(R.font.quicksand_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = QuickSand,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 30.sp
     )
     /* Other default text styles to override
     button = TextStyle(
@@ -26,3 +36,4 @@ val Typography = Typography(
     )
     */
 )
+
