94c85a40b884cd4f593ba64761e7ae5ba0e3fab6
Maxime Sontag <maxime_sontag@ens.univ-artois.fr>
2023-03-23 23:51:05 -0400

    calendrier avec l'ajout d'evenement avec un timepicker

--- app/src/main/java/ca/uqac/bubble/EventEditActivity.java
+++ app/src/main/java/ca/uqac/bubble/EventEditActivity.java
@@ -8,6 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Locale;
 
 import java.time.LocalTime;
 
@@ -18,18 +26,41 @@ public class EventEditActivity extends Activity
 
     private LocalTime time;
 
+    Button timeButton;
+    int hour, minute;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_edit);
         initWidgets();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            time = LocalTime.now();
-        }
+        time = LocalTime.of(12,00,0,0);
         eventDateTV.setText("Date: " + CalendarUtils.formattedDate(CalendarUtils.selectedDate));
         eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(time));
     }
+
+    public void popTimePicker(View view)
+    {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener()
+        {
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute)
+            {
+                hour = selectedHour;
+                minute = selectedMinute;
+                time = LocalTime.of(hour,minute,0,0);
+                eventTimeTV.setText("Time: " + CalendarUtils.formattedTime(time));
+            }
+        };
+
+        // int style = AlertDialog.THEME_HOLO_DARK;
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, /*style,*/ onTimeSetListener, hour, minute, true);
+
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
+    }
+
 
     private void initWidgets()
     {
