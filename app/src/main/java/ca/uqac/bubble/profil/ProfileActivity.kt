da7ffa6b09400d6b19e2c5cbeac293a1c5282506
Maxime Sontag <maxime_sontag@ens.univ-artois.fr>
2023-03-29 11:27:44 -0400

    Profil page sans changement image fonctionnel #6

--- app/src/main/java/ca/uqac/bubble/profil/ProfileActivity.kt
+++ app/src/main/java/ca/uqac/bubble/profil/ProfileActivity.kt
@@ -1,55 +1,144 @@
 package ca.uqac.bubble.profil
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
+import ca.uqac.bubble.MainActivity
 import ca.uqac.bubble.R
+import java.io.ByteArrayOutputStream
+import java.io.FileNotFoundException
+import java.io.IOException
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : ComponentActivity() {
 
-    companion object {
-        const val REQUEST_CODE_IMAGE = 1001
-    }
-
-    private lateinit var profileImage: ImageView
-    private lateinit var changePhotoButton: Button
     private lateinit var nameEditText: EditText
-    private lateinit var userName: String
+    private lateinit var profileImageView: ImageView
+    private lateinit var nameTextView: TextView
+    private lateinit var sharedPref: SharedPreferences
+    private var selectedImageUri: Uri? = null
+    private val PICK_IMAGE_REQUEST = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profil)
 
-        profileImage = findViewById(R.id.profile_image)
-        changePhotoButton = findViewById(R.id.change_photo_button)
-        nameEditText = findViewById(R.id.name_edit_text)
+        // Initialisation des vues
+        nameEditText = findViewById(R.id.nameEditText)
+        profileImageView = findViewById(R.id.profileImageView)
+        nameTextView = findViewById(R.id.nameTextView)
 
-        // Get the default user name from the string resources
-        userName = getString(R.string.default_name)
+        // Initialisation des préférences partagées
+        sharedPref = getSharedPreferences("MY_PREFS_NAME", Context.MODE_PRIVATE)
 
-        nameEditText.setOnEditorActionListener { _, _, _ ->
-            // Update the user name with the new name entered by the user
-            userName = nameEditText.text.toString()
-            true
+        // Récupération du nom d'utilisateur enregistré ou valeur par défaut
+        val userName = sharedPref.getString("USER_NAME", getString(R.string.default_name))
+
+        // Affichage du nom d'utilisateur
+        nameTextView.text = userName
+
+        // Récupération de l'image de profil enregistrée ou valeur par défaut
+        val profileImageString = sharedPref.getString("PROFILE_IMAGE", "")
+        if (!profileImageString.isNullOrEmpty()) {
+            profileImageView.setImageBitmap(decodeStringToImage(profileImageString))
         }
 
-        changePhotoButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_GET_CONTENT)
-            intent.type = "image/*"
-            startActivityForResult(intent, REQUEST_CODE_IMAGE)
+        // Gestionnaire de clics du bouton Valider
+        val saveButton: Button = findViewById(R.id.saveButton)
+        saveButton.setOnClickListener {
+            // Récupération du nouveau nom d'utilisateur
+            val newUserName = nameEditText.text.toString()
+
+            // Enregistrement du nouveau nom d'utilisateur dans les préférences partagées
+            with(sharedPref.edit()) {
+                putString("USER_NAME", newUserName)
+                apply()
+            }
+
+            // Enregistrement de la nouvelle image de profil dans les préférences partagées
+            if (selectedImageUri != null) {
+                val bitmap = getBitmapFromUri(selectedImageUri!!)
+                if (bitmap != null) {
+                    val profileImageString = encodeImageToString(bitmap)
+                    // Faire quelque chose avec profileImageString...
+                } else {
+                    // Gérer le cas où la valeur retournée par getBitmapFromUri est nulle...
+                }
+                with(sharedPref.edit()) {
+                    putString("PROFILE_IMAGE", profileImageString)
+                    apply()
+                }
+            }
+
+            // Démarrage d'une nouvelle activité
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
+
+        // Gestionnaire de clics de l'image de profil
+        profileImageView.setOnClickListener {
+            val galleryIntent =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galleryIntent, 1000)
+        }
+    }
+
+    // Convertit une image Bitmap en une chaîne encodée en base64
+    private fun encodeImageToString(bitmap: Bitmap): String {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        val byteArray = byteArrayOutputStream.toByteArray()
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_IMAGE && resultCode == Activity.RESULT_OK) {
-            // Update the profile image with the selected image
-            val imageUri = data?.data
-            profileImage.setImageURI(imageUri)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            // Récupération de l'image sélectionnée par l'utilisateur
+            val imageUri: Uri = data.data!!
+            val inputStream = contentResolver.openInputStream(imageUri)
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            inputStream?.close()
+
+            // Affichage de l'image dans l'ImageView
+            profileImageView.setImageBitmap(bitmap)
+
+            // Conversion de l'image en une chaîne de caractères encodée en Base64
+            val imageString = encodeImageToString(bitmap)
+
+            // Enregistrement de la chaîne encodée dans les préférences partagées
+            with(sharedPref.edit()) {
+                putString("USER_IMAGE", imageString)
+                apply()
+            }
+        }
+    }
+
+    fun decodeStringToImage(imageString: String): Bitmap {
+        val decodedBytes = Base64.decode(imageString, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+    }
+
+    private fun getBitmapFromUri(uri: Uri): Bitmap? {
+        return try {
+            val inputStream = contentResolver.openInputStream(uri)
+            BitmapFactory.decodeStream(inputStream)
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+            null
         }
     }
 }
