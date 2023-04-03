351e19d9b44a8856cd0caeed8736ebe602a21a8a
Paul NAUDIN <paul.naudin@epitech.eu>
2023-04-03 17:59:52 -0400

    Added the possibility to launch the pomodoro with an intent, added the cycles of pomodoro

--- app/src/main/java/ca/uqac/bubble/pomodoro/PomodoroActivity.kt
+++ app/src/main/java/ca/uqac/bubble/pomodoro/PomodoroActivity.kt
@@ -6,41 +6,54 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ca.uqac.bubble.R
-import ca.uqac.bubble.ui.theme.BubbleAppTheme
 
 class PomodoroActivity : ComponentActivity() {
 
     private lateinit var timeDisplay: TextView
+    private lateinit var phaseDisplay: TextView
+    private lateinit var cyclesDisplay: TextView
     private lateinit var pauseResumeButton: Button
     private lateinit var stopButton: Button
     private var timer: CountDownTimer? = null
     private var isRunning = false
     private var isPaused = false
+    private var isPomodoroPhase = true
     private var remainingTime = 120_000L // 120 seconds
+    private var pomodoroPhase = 1500000L // 25 minutes in milliseconds
+    private var breakPhase = 300000L // 5 minutes in milliseconds
+    private var numberCycles = 4 // 4 cycles per pomodoro session by default
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pomodoro)
 
+        val extras = intent.extras
+        if (extras != null) {
+            pomodoroPhase = extras.getLong("pomodoroPhase", pomodoroPhase)
+            breakPhase = extras.getLong("breakPhase", breakPhase)
+            numberCycles = extras.getInt("numberCycles", numberCycles)
+        }
+
+        remainingTime = pomodoroPhase
+
         timeDisplay = findViewById(R.id.time_display)
         pauseResumeButton = findViewById(R.id.pause_resume_button)
         stopButton = findViewById(R.id.stop_button)
+        phaseDisplay = findViewById(R.id.phase_display)
+        cyclesDisplay = findViewById(R.id.cycles_display)
 
         updateTimerDisplay()
+        updatePhaseDisplay()
+        updateCyclesDisplay()
 
         pauseResumeButton.setOnClickListener {
-            if (isRunning && !isPaused) {
+            if (isRunning) {
                 pauseTimer()
                 pauseResumeButton.text = "Resume"
                 stopButton.visibility = View.VISIBLE
                 isPaused = true
-            } else if (isRunning && isPaused) {
+            } else if (isRunning) {
                 resumeTimer()
                 pauseResumeButton.text = "Pause"
                 stopButton.visibility = View.GONE
@@ -69,21 +82,26 @@ class PomodoroActivity : ComponentActivity() {
             }
 
             override fun onFinish() {
-                stopTimer()
-                isRunning = false
-                isPaused = false
-                finish()
+                if (isPomodoroPhase && numberCycles > 0) {
+                    isPomodoroPhase = false
+                    remainingTime = breakPhase
+                    updatePhaseDisplay()
+                } else if (!isPomodoroPhase && numberCycles > 0) {
+                    isPomodoroPhase = true
+                    remainingTime = pomodoroPhase
+                    numberCycles--
+                    updatePhaseDisplay()
+                    updateCyclesDisplay()
+                } else {
+                    stopTimer()
+                    isRunning = false
+                    isPaused = false
+                    finish()
+                }
             }
         }
         timer?.start()
         isRunning = true
-    }
-
-    private fun updateTimerDisplay(timeDisplay: TextView, remainingTime: Long) {
-        val seconds = (remainingTime / 1000) % 60
-        val minutes = (remainingTime / (1000 * 60)) % 60
-        val hours = remainingTime / (1000 * 60 * 60)
-        timeDisplay.text = String.format("%02d:%02d:%02d", hours, minutes, seconds)
     }
 
     private fun pauseTimer() {
@@ -104,5 +122,19 @@ class PomodoroActivity : ComponentActivity() {
         val hours = remainingTime / (1000 * 60 * 60)
         timeDisplay.text = String.format("%02d:%02d:%02d", hours, minutes, seconds)
     }
+
+    private fun updatePhaseDisplay() {
+        if (isPomodoroPhase) {
+            phaseDisplay.text = "Pomodoro phase"
+        } else {
+            phaseDisplay.text = "Break phase"
+        }
+    }
+
+    private fun updateCyclesDisplay() {
+        cyclesDisplay.text = "Cycles left: $numberCycles"
+    }
+
+
 
 }
